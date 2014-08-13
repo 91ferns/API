a423e81a3854699be133057b62b7eaca734b2fe5
Stephen Parente <stephen@ingenuitydesign.com>
2014-08-13 17:50:24 -0400

    Added mongodb dependencies

--- project/build.scala
+++ project/build.scala
@@ -31,6 +31,7 @@ object FerntasticApiBuild extends Build {
         "org.scalatra" %% "scalatra-json" % "2.3.0",
         "org.json4s"   %% "json4s-jackson" % "3.2.9",
         "org.scalaj" %% "scalaj-http" % "0.3.16",
+        "org.mongodb" %% "casbah" % "2.7.3",
         "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
